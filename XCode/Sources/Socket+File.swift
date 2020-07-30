bca6266d74227372a5e6395759dcdb35a389d8d4
Jonathan Crooke <jonathan.crooke@zalando.de>
2020-07-30 17:26:41 +0200

    Fix buffer access
    ./swifter/XCode/Sources/Socket+File.swift:26:53: warning: inout expression creates a temporary pointer, but argument #1 should be a pointer that outlives the call to '+'
                        let writeResult = write(target, &buffer + writeCounter, readResult - writeCounter)
                                                        ^~~~~~~
    ./swifter/XCode/Sources/Socket+File.swift:26:53: note: implicit argument conversion from '[UInt8]' to 'UnsafeRawPointer' produces a pointer valid only for the duration of the call to '+'
                        let writeResult = write(target, &buffer + writeCounter, readResult - writeCounter)
                                                        ^~~~~~~
    ./swifter/XCode/Sources/Socket+File.swift:26:53: note: use the 'withUnsafeBytes' method on Array in order to explicitly convert argument to buffer pointer valid for a defined scope
                        let writeResult = write(target, &buffer + writeCounter, readResult - writeCounter)
                                                        ^
    ./swifter/XCode/Sources/Socket+File.swift:26:61: error: generic parameter 'Element' could not be inferred
                        let writeResult = write(target, &buffer + writeCounter, readResult - writeCounter)

--- XCode/Sources/Socket+File.swift
+++ XCode/Sources/Socket+File.swift
@@ -21,9 +21,9 @@ import Foundation
             var writeCounter = 0
             while writeCounter < readResult {
                 #if os(Linux)
-                    let writeResult = send(target, &buffer + writeCounter, readResult - writeCounter, Int32(MSG_NOSIGNAL))
+                    let writeResult = send(target, &buffer[writeCounter], readResult - writeCounter, Int32(MSG_NOSIGNAL))
                 #else
-                    let writeResult = write(target, &buffer + writeCounter, readResult - writeCounter)
+                    let writeResult = write(target, &buffer[writeCounter], readResult - writeCounter)
                 #endif
                 guard writeResult > 0 else {
                     return Int32(writeResult)
